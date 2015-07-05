79a9cd26c16b0a1b5820e77208dab1048a73e7b4
Eric Bridgeford <misterbr@gmail.com>
2015-07-05 17:51:45 -0400

    comments

--- helloworld.rs
+++ helloworld.rs
@@ -1,3 +1,8 @@
+/*
+This is new in a minor way. I'm used to C++ and Java where there is a lot of stuff preceding the print statement.
+Rust calls this a Macros which I've heard talked about in C but have never used. Which I guess doesn't matter since
+the docs say Rust's are different from C's.
+*/
 fn main() {
 println!("Hello, world!");
 }
