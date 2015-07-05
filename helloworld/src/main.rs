3be2ea419755c0e574ae77736616a91e75577513
Eric Bridgeford <misterbr@gmail.com>
2015-07-05 18:04:04 -0400

    cargo comments2

--- helloworld/src/main.rs
+++ helloworld/src/main.rs
@@ -2,6 +2,8 @@
 This is new in a minor way. I'm used to C++ and Java where there is a lot of stuff preceding the print statement.
 Rust calls this a Macros which I've heard talked about in C but have never used. Which I guess doesn't matter since
 the docs say Rust's are different from C's.
+
+My very limited experience with make makes Cargo seem like a dream
 */
 fn main() {
 println!("Hello, world!");
