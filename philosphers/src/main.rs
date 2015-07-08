b5c2b86a9339e16809b42a32077f8a7b049db0fa
Eric Bridgeford <misterbr@gmail.com>
2015-07-07 22:17:54 -0400

    threads to wait

--- philosphers/src/main.rs
+++ philosphers/src/main.rs
@@ -1,3 +1,37 @@
+use std::thread;
+
+struct Philospher {
+    name: String,
+}
+
+impl Philospher {
+    fn new(name: &str) -> Philospher {
+        Philospher {
+            name: name.to_string(),
+        }
+}
+
+    fn eat(&self) {
+        println!("{} is eating", self.name);
+
+        thread::sleep_ms(1000);
+
+        println!("{} is done eating.", self.name);
+        }
+    }
+
+
 fn main() {
-    println!("Hello, world!");
+
+let philosphers = vec![
+        Philospher::new("Baruch Spinoza"),
+        Philospher::new("Gilles Deleuze"),
+        Philospher::new("Karl Marx"),
+        Philospher::new("Friedrich Nietzsche"),
+        Philospher::new("Michel Foucault"),
+        ];
+
+        for p in &philosphers {
+            p.eat();
+        }
 }
